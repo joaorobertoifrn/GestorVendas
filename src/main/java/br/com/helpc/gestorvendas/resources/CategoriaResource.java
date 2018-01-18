6ee8bd18a2901e8ea0c863b75bf99baf044cbc64
João Roberto <joao_roberto@msn.com>
2018-01-18 20:35:26 -0300

    REST Testado

--- src/main/java/br/com/helpc/gestorvendas/resources/CategoriaResource.java
+++ src/main/java/br/com/helpc/gestorvendas/resources/CategoriaResource.java
@@ -1,5 +1,16 @@
 package br.com.helpc.gestorvendas.resources;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value="/categorias")
 public class CategoriaResource {
+	
+	@RequestMapping(method=RequestMethod.GET)
+	public String listar() {
+		return "REST está funcionando";
+	}
 
 }
