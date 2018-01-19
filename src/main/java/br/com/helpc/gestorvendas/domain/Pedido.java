e4d86984ccc61a508d9f9f18fe3e99a211fec865
João Roberto <joao_roberto@msn.com>
2018-01-19 20:36:43 -0300

    ItemPedido e ItemPedidoPK

--- src/main/java/br/com/helpc/gestorvendas/domain/Pedido.java
+++ src/main/java/br/com/helpc/gestorvendas/domain/Pedido.java
@@ -2,6 +2,8 @@ package br.com.helpc.gestorvendas.domain;
 
 import java.io.Serializable;
 import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
 
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
@@ -10,6 +12,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 
 @Entity
@@ -33,6 +36,9 @@ public class Pedido implements Serializable {
 	@ManyToOne
 	@JoinColumn(name="endereco_de_entrega_id")
 	private Endereco enderecoDeEntrega;
+	
+	@OneToMany(mappedBy="id.pedido")
+	private Set<ItemPedido> itens = new HashSet<>();
 	
 	public Integer getId() {
 		return id;
@@ -74,10 +80,14 @@ public class Pedido implements Serializable {
 		this.enderecoDeEntrega = enderecoDeEntrega;
 	}
 
-	public static long getSerialversionuid() {
-		return serialVersionUID;
+	public Set<ItemPedido> getItens() {
+		return itens;
 	}
 
+	public void setItens(Set<ItemPedido> itens) {
+		this.itens = itens;
+	}	
+	
 	public Pedido() {
 
 	}
--- src/main/java/br/com/helpc/gestorvendas/domain/Produto.java
+++ src/main/java/br/com/helpc/gestorvendas/domain/Produto.java
@@ -2,7 +2,9 @@ package br.com.helpc.gestorvendas.domain;
 
 import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -11,6 +13,7 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
+import javax.persistence.OneToMany;
 
 import com.fasterxml.jackson.annotation.JsonBackReference;
 
@@ -33,6 +36,18 @@ public class Produto implements Serializable {
 	joinColumns = @JoinColumn(name="produto_id"), 
 	inverseJoinColumns = @JoinColumn(name="categoria_id"))
 	private List<Categoria> categorias = new ArrayList<>();
+	
+	@OneToMany(mappedBy="id.produto")
+	private Set<ItemPedido> itens = new HashSet<>();
+	
+	public List<Pedido> getPedidos() {
+		List<Pedido> lista = new ArrayList<>();
+		for (ItemPedido x: itens) {
+			lista.add(x.getPedido());
+		}
+		
+		return lista;
+	}
 	
 	public Integer getId() {
 		return id;
@@ -65,7 +80,15 @@ public class Produto implements Serializable {
 	public void setCategorias(List<Categoria> categorias) {
 		this.categorias = categorias;
 	}
+	
+	public Set<ItemPedido> getItens() {
+		return itens;
+	}
 
+	public void setItens(Set<ItemPedido> itens) {
+		this.itens = itens;
+	} 
+	
 	public Produto() {
 		
 	}
@@ -100,6 +123,6 @@ public class Produto implements Serializable {
 		} else if (!id.equals(other.id))
 			return false;
 		return true;
-	} 
-	
+	}
+
 }
