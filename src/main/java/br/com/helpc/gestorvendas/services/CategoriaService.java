d38ea8b3bf04eeb61a3e7226ee920c97634f196d
João Roberto <joao_roberto@msn.com>
2018-01-20 10:12:55 -0300

    Lista de Categorias com Paginação e parametros opcionais

--- src/main/java/br/com/helpc/gestorvendas/services/CategoriaService.java
+++ src/main/java/br/com/helpc/gestorvendas/services/CategoriaService.java
@@ -4,6 +4,9 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import br.com.helpc.gestorvendas.domain.Categoria;
@@ -49,7 +52,10 @@ public class CategoriaService {
 		return repo.findAll();
 	}
 	
-	
+	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
+		PageRequest pageRequest = new PageRequest(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		return repo.findAll(pageRequest);
+	}
 	
 
 }
