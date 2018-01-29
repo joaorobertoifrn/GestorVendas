e8112629e75c431a7e832e53d06189aca728501b
João Roberto <joao_roberto@msn.com>
2018-01-29 17:41:31 -0300

    Criado profile de Teste e de Desenvolvimento e Produção

---
+++ src/main/java/br/com/helpc/gestorvendas/config/DevConfig.java
@@ -0,0 +1,26 @@
+package br.com.helpc.gestorvendas.config;
+
+import java.text.ParseException;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+@Configuration
+@Profile("dev")
+public class DevConfig {
+	
+	@Value("${spring.jpa.hibernate.ddl-auto}")
+	private String strategy;
+	
+	@Bean
+	public boolean instantiateDatabase() throws ParseException {
+		
+		if (!"create".equals(strategy)) {
+			return false;
+		}
+		return true;
+	}
+	
+}
---
+++ src/main/java/br/com/helpc/gestorvendas/config/TestConfig.java
@@ -0,0 +1,10 @@
+package br.com.helpc.gestorvendas.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+@Configuration
+@Profile("test")
+public class TestConfig {
+
+}
