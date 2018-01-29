fe125f3e9850e7a9aa07dbe41a9d44356917801d
João Roberto <joao_roberto@msn.com>
2018-01-29 17:11:00 -0300

    Inserindo Pedido

--- src/main/java/br/com/helpc/gestorvendas/domain/PagamentoComCartao.java
+++ src/main/java/br/com/helpc/gestorvendas/domain/PagamentoComCartao.java
@@ -2,9 +2,12 @@ package br.com.helpc.gestorvendas.domain;
 
 import javax.persistence.Entity;
 
+import com.fasterxml.jackson.annotation.JsonTypeName;
+
 import br.com.helpc.gestorvendas.domain.enums.EstadoPagamento;
 
 @Entity
+@JsonTypeName("pagamentoComCartao")
 public class PagamentoComCartao extends Pagamento {
 	
 	private static final long serialVersionUID = 1L;
--- src/main/java/br/com/helpc/gestorvendas/services/PedidoService.java
+++ src/main/java/br/com/helpc/gestorvendas/services/PedidoService.java
@@ -1,10 +1,19 @@
 package br.com.helpc.gestorvendas.services;
 
+import java.util.Date;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import br.com.helpc.gestorvendas.domain.ItemPedido;
+import br.com.helpc.gestorvendas.domain.PagamentoComBoleto;
 import br.com.helpc.gestorvendas.domain.Pedido;
+import br.com.helpc.gestorvendas.domain.enums.EstadoPagamento;
+import br.com.helpc.gestorvendas.repositories.ClienteRepository;
+import br.com.helpc.gestorvendas.repositories.ItemPedidoRepository;
+import br.com.helpc.gestorvendas.repositories.PagamentoRepository;
 import br.com.helpc.gestorvendas.repositories.PedidoRepository;
+import br.com.helpc.gestorvendas.repositories.ProdutoRepository;
 import br.com.helpc.gestorvendas.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -13,11 +22,48 @@ public class PedidoService {
 	@Autowired
 	private PedidoRepository repo;
 	
+	@Autowired
+	private BoletoService boletoService;
+	
+	@Autowired
+	private PagamentoRepository pagamentoRepository;
+	
+	@Autowired
+	private ProdutoRepository produtoRepository;
+	
+	@Autowired
+	private ItemPedidoRepository itemPedidoRepository;
+	
+	@Autowired
+	private ClienteRepository clienteRepository;
+	
 	public Pedido find(Integer id) {
 		Pedido obj = repo.findOne(id);
 		if (obj == null) {
 			throw new ObjectNotFoundException("Objeto não encontrado! Id: "+id+ " , Tipo: "+ Pedido.class.getName());
 		}
+		return obj;
+	}
+
+	public Pedido insert(Pedido obj) {
+		obj.setId(null);
+		obj.setInstante(new Date());
+		obj.setCliente(clienteRepository.findOne(obj.getCliente().getId()));
+		obj.getPagamento().setEstado(EstadoPagamento.PENDENTE);
+		obj.getPagamento().setPedido(obj);
+		if (obj.getPagamento() instanceof PagamentoComBoleto) {
+			PagamentoComBoleto pagto = (PagamentoComBoleto) obj.getPagamento();
+			boletoService.preencherPagamentoComBoleto(pagto, obj.getInstante());
+		}
+		obj = repo.save(obj);
+		pagamentoRepository.save(obj.getPagamento());
+		for (ItemPedido ip : obj.getItens()) {
+			ip.setDesconto(0.0);
+			ip.setProduto(produtoRepository.findOne(ip.getProduto().getId()));
+			ip.setPreco(ip.getProduto().getPreco());
+			ip.setPedido(obj);
+		}
+		itemPedidoRepository.save(obj.getItens());
 		return obj;
 	}
 }
