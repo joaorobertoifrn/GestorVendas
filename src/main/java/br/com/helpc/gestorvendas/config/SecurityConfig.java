1a3288f6bf9382304c921fded08686b1f61012eb
joao_ <joao_@Gracielly-PC>
2018-02-18 23:14:24 -0300

    Salvando Perfis de Usuarios na Base de Dados

--- src/main/java/br/com/helpc/gestorvendas/config/SecurityConfig.java
+++ src/main/java/br/com/helpc/gestorvendas/config/SecurityConfig.java
@@ -18,7 +18,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	
 	private static final String[] PUBLIC_MATCHERS_GET = { 
 			"/produtos/**",
-			"/categorias/**" };
+			"/categorias/**",
+			"/clientes/**"
+			};
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
--- src/main/java/br/com/helpc/gestorvendas/domain/Cliente.java
+++ src/main/java/br/com/helpc/gestorvendas/domain/Cliente.java
@@ -5,19 +5,22 @@ import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 import javax.persistence.CascadeType;
 import javax.persistence.CollectionTable;
 import javax.persistence.Column;
 import javax.persistence.ElementCollection;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
 
-
 import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import br.com.helpc.gestorvendas.domain.enums.Perfil;
 import br.com.helpc.gestorvendas.domain.enums.TipoCliente;
 
 @Entity
@@ -48,6 +51,10 @@ public class Cliente implements Serializable {
 	@ElementCollection
 	@CollectionTable(name="telefone")
 	private Set<String> telefones = new HashSet<>();
+	
+	@ElementCollection(fetch=FetchType.EAGER)
+	@CollectionTable(name="perfis")
+	private Set<Integer> perfis = new HashSet<>();
 	
 	@OneToMany(mappedBy="cliente")
 	@JsonIgnore
@@ -100,7 +107,15 @@ public class Cliente implements Serializable {
 	public void setSenha(String senha) {
 		this.senha = senha;
 	}
+	
+	public Set<Perfil> getPerfis(){
+		return perfis.stream().map(x -> Perfil.toEnum(x)).collect(Collectors.toSet());
+	}
 
+	public void addPerfil(Perfil perfil) {
+		perfis.add(perfil.getCod());
+	}
+	
 	public List<Endereco> getEnderecos() {
 		return enderecos;
 	}
@@ -126,6 +141,7 @@ public class Cliente implements Serializable {
 	}
 	
 	public Cliente() {
+		addPerfil(Perfil.CLIENTE);
 		
 	}
 
@@ -137,6 +153,7 @@ public class Cliente implements Serializable {
 		this.cpfOuCnpj = cpfOuCnpj;
 		this.tipo = (tipo==null) ? null: tipo.getCod();
 		this.senha = senha;
+		addPerfil(Perfil.CLIENTE);
 	}
 
 
