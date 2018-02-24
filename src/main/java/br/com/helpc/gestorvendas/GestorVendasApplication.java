fe8be3a0d0cfc617609c4bdca0d0e11f7d00b96b
joao_ <joao_@Gracielly-PC>
2018-02-24 20:56:34 -0300

    Salvando primeiro arquivo no S3

--- src/main/java/br/com/helpc/gestorvendas/GestorVendasApplication.java
+++ src/main/java/br/com/helpc/gestorvendas/GestorVendasApplication.java
@@ -1,12 +1,26 @@
 package br.com.helpc.gestorvendas;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class GestorVendasApplication {
+import br.com.helpc.gestorvendas.services.S3Service;
 
+@SpringBootApplication
+public class GestorVendasApplication implements CommandLineRunner {
+
+	@Autowired
+	private S3Service s3Service;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(GestorVendasApplication.class, args);
 	}
+
+	@Override
+	public void run(String... arg0) throws Exception {
+		s3Service.uploadFile("C:\\Temp\\imagens\\cp1.jpg");
+		
+	}
+	
 }
